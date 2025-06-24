7befd2cb7afe3315e02330e4b2faf95425b6225c
piovke <115153510+piovke@users.noreply.github.com>
2025-06-24 22:55:59 +0200

    Update main.py
    zmiana przykladowego poziomu

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 from solver import *
 
 poziom = Plansza([
-    Probowka(['cziel','bez','malin','ziel']),
-    Probowka(['pom','gran','zol','fiol']),
-    Probowka(['fiol','pizdz','gran','braz']),
-    Probowka(['pizdz','malin','malin','roz']),
-    Probowka(['roz','bez','gran','pom']),
-    Probowka(['bez','nieb','pizdz','zol']),
-    Probowka(['cziel','zol','braz','pizdz']),
-    Probowka(['pom','cziel','fiol','ziel']),
-    Probowka(['nieb','braz','pom','nieb']),
-    Probowka(['braz','roz','malin','nieb']),
-    Probowka(['bez','zol','gran','roz']),
-    Probowka(['ziel','cziel','ziel','fiol']),
-    Probowka([]),
+    Probowka(['zolty','bezowy','niebieski','czerwony']),
+    Probowka(['bezowy','zolty','niebieski']),
+    Probowka(['czerwony','pomaranczowy','rozowy','pomaranczowy']),
+    Probowka(['bezowy','czerwony','pomaranczowy','niebieski']),
+    Probowka(['czerwony','zielony','zielony','rozowy']),
+    Probowka(['zielony','zolty','pomaranczowy','niebieski']),
+    Probowka(['zolty','rozowy','zielony','bezowy']),
+    Probowka(['rozowy']),
     Probowka([]),
 ])
+
 
 print('rozwiązywanie...\n')
 rozwiazanie = solve(poziom)
